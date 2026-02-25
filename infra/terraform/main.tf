46a5fa107ea0a49211f9ea17aaed1c7b65fc962d
croll83 <monaco.marco83@gmail.com>
2026-02-25 01:45:06 +0100

    fix

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,33 +1,24 @@
 terraform {
   required_providers {
     proxmox = {
-      source  = "Telmate/proxmox"
-      version = "~> 2.9"
+      source  = "bpg/proxmox"
+      version = "~> 0.97"
     }
   }
 }
 
 provider "proxmox" {
-  pm_api_url          = var.proxmox_api_url
-  pm_api_token_id     = var.proxmox_api_token_id
-  pm_api_token_secret = var.proxmox_api_token_secret
-  pm_tls_insecure     = true
-  pm_debug            = true
-  pm_log_enable       = true
-  pm_log_file         = "terraform-plugin-proxmox.log"
+  endpoint  = var.proxmox_api_url
+  api_token = var.proxmox_api_token
+  insecure  = true
 }
 
 variable "proxmox_api_url" {
-  default = "https://192.168.1.49:8006/api2/json"
+  default = "https://192.168.1.49:8006/"
 }
 
-variable "proxmox_api_token_id" {
-  description = "API token ID (es. root@pam!terraform)"
-  default     = "root@pam!terraform"
-}
-
-variable "proxmox_api_token_secret" {
-  description = "API token secret"
+variable "proxmox_api_token" {
+  description = "API token (es. root@pam!terraform=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx)"
   sensitive   = true
 }
 
@@ -39,39 +30,56 @@ variable "ssh_public_key" {
   default = ""
 }
 
-resource "proxmox_lxc" "tts_calibration" {
-  hostname    = "tts-calibration"
-  target_node = var.proxmox_node
-  ostemplate  = "local:vztmpl/debian-12-standard_12.7-1_amd64.tar.zst"
+resource "proxmox_virtual_environment_container" "tts_calibration" {
+  node_name    = var.proxmox_node
+  unprivileged = true
+  started      = true
+  tags         = ["jarvis", "calibration", "temp"]
 
-  cores  = 2
-  memory = 2048
-  swap   = 512
-
-  rootfs {
-    storage = "local-lvm"
-    size    = "10G"
+  operating_system {
+    template_file_id = "local:vztmpl/debian-12-standard_12.7-1_amd64.tar.zst"
+    type             = "debian"
   }
 
-  network {
-    name   = "eth0"
-    bridge = "vmbr0"
-    ip     = "dhcp"
+  cpu {
+    cores = 2
+  }
+
+  memory {
+    dedicated = 2048
+    swap      = 512
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    size         = 10
   }
 
   features {
-    nesting = true  # necessario per Docker
+    nesting = true
     keyctl  = true
   }
 
-  start        = true
-  unprivileged = true
+  initialization {
+    hostname = "tts-calibration"
 
-  ssh_public_keys = var.ssh_public_key
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
 
-  tags = "jarvis,calibration,temp"
+    user_account {
+      keys = var.ssh_public_key != "" ? [var.ssh_public_key] : []
+    }
+  }
+
+  network_interface {
+    name   = "eth0"
+    bridge = "vmbr0"
+  }
 }
 
 output "lxc_id" {
-  value = proxmox_lxc.tts_calibration.vmid
+  value = proxmox_virtual_environment_container.tts_calibration.vm_id
 }
